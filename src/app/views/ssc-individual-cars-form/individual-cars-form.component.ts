c665fc5fdd6f68ad59bc3fc0072f6dea9f6734c3
Zsolt <lordmairtis@gmail.com>
2018-03-06 22:08:38 +0100

    Deploy fix (#2)
    * Dev mode heroku deploy (#1)
    
    * fix build pls
    
    * fix deploy
    
    * fix error message
    
    * final fixes to deploy to heroku
    
    * sending token id to backend
    
    * fix ngFor on individual cars component

--- src/app/views/ssc-individual-cars-form/individual-cars-form.component.ts
+++ src/app/views/ssc-individual-cars-form/individual-cars-form.component.ts
@@ -1,15 +1,13 @@
 /**
  * Form for cars put in individually
  */
-import {
-    Component
-} from '@angular/core';
-import {OnDestroy} from '@angular/core';
-import {AppState} from '../../reducers';
+import {Component, OnDestroy} from '@angular/core';
+import {AppState, selectAuth2} from '../../reducers';
 import {Store} from '@ngrx/store';
 import {Subscription} from 'rxjs/Subscription';
-import {SetAction, RemoveAction, GetAction} from '../../actions';
+import {GetAction, SetAction} from '../../actions';
 import {SpinnerService} from '../../services/spinner.service';
+import 'rxjs/add/operator/take';
 
 
 @Component({
@@ -20,14 +18,22 @@ import {SpinnerService} from '../../services/spinner.service';
 export class IndividualCarsFormComponent implements OnDestroy {
     uris: string[];
     spinner = false;
+    private idToken = '';
     private subscription: Subscription = new Subscription();
 
     constructor(private store: Store<AppState>, private spinnerService: SpinnerService) {
-        this.subscription.add(
-            store.select(state => state.individualCars.urls).subscribe((urls) => {
+        store.select(state => state.individualCars.urls).take(1).subscribe((urls) => {
                 this.uris = urls;
-            })
+            }
         );
+
+        this.subscription.add(this.store.select(selectAuth2).subscribe((auth2) => {
+            if(auth2.currentUser) {
+                const currentUser = auth2.currentUser.get();
+                const isSignedIn = currentUser.isSignedIn();
+                this.idToken = isSignedIn ? currentUser.getAuthResponse().id_token : '';
+            }
+        }));
         this.subscription.add(
             spinnerService.subscribe(waiting => {
                 this.spinner = waiting;
@@ -39,16 +45,16 @@ export class IndividualCarsFormComponent implements OnDestroy {
         this.subscription.unsubscribe();
     }
 
-    addNewUrl(event: any, index: number) {
-        this.store.dispatch(new SetAction(event.target.value, index));
+    addNewUrl(uri: string, index: number) {
+        this.store.dispatch(new SetAction(uri, index));
     }
 
     getCarData() {
         this.spinner = true;
-        this.store.dispatch(new GetAction(this.uris));
+        this.store.dispatch(new GetAction(this.uris, this.idToken));
     }
 
-    trackByIndex(elem, index) {
+    trackByIndex(index: number, obj: any): any {
         return index;
     }
 
