66c0f6385cf895fac311639ab17edd015b742cc2
Deak Zsolt <lordmairtis@gmail.com>
2018-04-08 21:59:00 +0200

    last try

--- src/app/components/ssc-car-data-table/car-data-table.component.ts
+++ src/app/components/ssc-car-data-table/car-data-table.component.ts
@@ -8,7 +8,6 @@ import {AuthState} from '../../reducers/auth/auth.reducer';
 import {GetSavedCarDataAction} from '../../actions';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
-import 'rxjs/add/operator/distinctUntilChanged';
 
 
 @Component({
@@ -31,8 +30,8 @@ export class CarDataTableComponent implements OnDestroy, OnInit {
 			this.carData = carDataState.cars.sort((car1, car2) => car2.worth - car1.worth);
 			this.cdRef.detectChanges();
 		}));
-		this.subscription.add(this.store.select(selectAuthState).distinctUntilChanged().subscribe((authState: AuthState) => {
-			if(authState.isSignedIn) {
+		this.subscription.add(this.store.select(selectAuthState).subscribe((authState: AuthState) => {
+			if(authState.isSignedIn && !this.carData.length) {
 				this.store.dispatch(new GetSavedCarDataAction(authState.idToken));
 			}
 			this.idToken = authState.idToken || '';
