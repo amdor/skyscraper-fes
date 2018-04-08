f963d5302089cbaf000ec1465d857f56185bee8d
Deak Zsolt <lordmairtis@gmail.com>
2018-04-08 22:08:54 +0200

    Revert "what's happening on prod?"
    This reverts commit 7139619bc7924cab67b3e4a4b5b5f8c607623020.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,8 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-// export const environment = {
-// 	production: false,
-//     bes: "http://127.0.0.1:5000/",
-// 	savedCarsEndpoint: "http://127.0.0.1:5000/saved-cars"
-// };
-
 export const environment = {
-	production: true,
-	bes: "https://skyscraper-bes.herokuapp.com/",
-	savedCarsEndpoint: "https://skyscraper-bes.herokuapp.com/saved-cars"
+	production: false,
+    bes: "http://127.0.0.1:5000/",
+	savedCarsEndpoint: "http://127.0.0.1:5000/saved-cars"
 };
-
