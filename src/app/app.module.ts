d38f1afe055200aab4cf183d22faa7c4c093f08c
Deak Zsolt <lordmairtis@gmail.com>
2018-05-03 17:19:18 +0200

    new routing so all components get auth data empty faq added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {BrowserModule, DomSanitizer} from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
@@ -10,11 +10,7 @@ import {ROUTES} from './app.routes';
 import {AppComponent} from './app.component';
 import {CarDataTableComponent} from './components/ssc-car-data-table/car-data-table.component';
 import {IndividualCarsFormComponent} from './views/ssc-individual-cars-form/individual-cars-form.component';
-import {
-	individualCarsFormReducer,
-	carDataTableReducer,
-	authReducer
-} from './reducers';
+import {authReducer, carDataTableReducer, individualCarsFormReducer} from './reducers';
 import {CarDataEffects} from './effects/car-data-effects';
 import {SpinnerService} from './services/spinner.service';
 import {SscNavbarComponent} from './components/ssc-navbar/ssc-navbar.component';
@@ -25,13 +21,17 @@ import {LocalStorageService} from './services/local-storage.service';
 import {SavedCarsComponent} from './views/ssc-saved-cars/saved-cars.component';
 import {SscNotificationComponent} from './components/ssc-notification/ssc-notification.component';
 import {SscNotificationService} from './services/ssc-notification.service';
+import {FaqComponent} from './views/ssc-faq/faq.component';
+import {MainComponent} from './views/ssc-main/main.component';
 
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		CarDataTableComponent,
+		FaqComponent,
 		IndividualCarsFormComponent,
+		MainComponent,
 		SavedCarsComponent,
 		SscNavbarComponent,
 		SscNotificationComponent
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,18 @@ import { Routes } from '@angular/router';
 import { IndividualCarsFormComponent } from './views/ssc-individual-cars-form/individual-cars-form.component';
 import { GoogleAuthResolver } from './resolvers';
 import {SavedCarsComponent} from './views/ssc-saved-cars/saved-cars.component';
+import {MainComponent} from './views/ssc-main/main.component';
+import {FaqComponent} from './views/ssc-faq/faq.component';
 
 export const ROUTES: Routes = [
-    { path: '',         component: IndividualCarsFormComponent,    resolve: {auth2: GoogleAuthResolver}},
-    { path: 'saved',    component: SavedCarsComponent,    resolve: {auth2: GoogleAuthResolver}}
+    {
+        path: '',
+        component: MainComponent,
+        resolve: {auth2: GoogleAuthResolver},
+        children: [
+			{ path: '',    component: IndividualCarsFormComponent},
+			{ path: 'saved',    component: SavedCarsComponent},
+			{ path: 'faq',      component: FaqComponent}
+        ]
+    }
 ];
