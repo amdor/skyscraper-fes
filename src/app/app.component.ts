0ce2d499c6cc0a9e2c2ccdc3b61fb3433f5f1943
Deak Zsolt <lordmairtis@gmail.com>
2018-05-19 11:20:29 +0200

    detecting locale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,43 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {Store} from '@ngrx/store';
 import {AppState} from './reducers';
+import {SetLanguageAction} from './actions/language.actions';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 	constructor(private translate: TranslateService, private store: Store<AppState>) {
 		// this language will be used as a fallback when a translation isn't found in the current language
-		translate.setDefaultLang('hu');
+		if(['en', 'hu'].includes(navigator.language)) {
+			translate.setDefaultLang(navigator.language);
+			this.store.dispatch(new SetLanguageAction(navigator.language));
+		} else if(navigator.languages.length) {
+			let lang = 'hu';
+			switch(true) {
+				case navigator.languages.includes('hu'):
+					lang = 'hu';
+					break;
+				case navigator.languages.some((language: string) => {
+					return language.includes('en');
+				}):
+					lang = 'en';
+					break;
+				default:
+					lang = 'en';
+			}
+			translate.setDefaultLang(lang);
+			this.store.dispatch(new SetLanguageAction(lang));
+		} else {
+			translate.setDefaultLang('en');
+			this.store.dispatch(new SetLanguageAction('en'));
+		}
 
 		// the lang to use, if the lang isn't available, it will use the current loader to get them
-		this.store.select((state: AppState) => state.language).subscribe((newLang:string) => {
+		this.store.select((state: AppState) => state.language).subscribe((newLang: string) => {
 			translate.use(newLang);
 		});
 	}
